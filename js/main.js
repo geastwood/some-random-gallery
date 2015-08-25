39e75d44524b1d96b0b4b716e6989e1a4ebc3a0d
Fei Liu <liu.feiwood@gmail.com>
2015-08-25 18:28:25 +0200

    optimize for minification

--- js/main.js
+++ js/main.js
@@ -2,30 +2,33 @@ var GalleryManager = (function(Q) {
 
     var Gallery = function(el) {
         this.container = el;
-        this.imageEls = this.container.all('.gallery-content > li');
-        this.indicatorEl = this.container.find('.indicator');
-        this.leftEl = this.container.find('.move-left');
-        this.rightEl = this.container.find('.move-right');
+        this.imageEls = el.all('.gallery-content > li');
+        this.indicatorEl = el.find('.indicator');
+        this.leftEl = el.find('.move-left');
+        this.rightEl = el.find('.move-right');
         this.update();
         this.attachEvent();
     };
 
-    Gallery.prototype.move = function(direction) {
+    var proto = Gallery.prototype;
+
+    proto.move = function(direction) {
         var currentIndex = Number(this.container.data('current')) - 1,
             currentItem = this.imageEls[currentIndex],
-            nextItem = null;
+            nextItem = null,
+            imageEls = this.imageEls;
 
         if (direction === 'right') {
-            nextItem = this.imageEls[currentIndex + 1] || this.imageEls[0];
+            nextItem = imageEls[currentIndex + 1] || imageEls[0];
         } else {
-            nextItem = this.imageEls[currentIndex - 1] || this.imageEls[this.imageEls.length - 1];
+            nextItem = imageEls[currentIndex - 1] || imageEls[imageEls.length - 1];
         }
 
         this.update(nextItem);
     };
 
-    Gallery.prototype.updateMeta = function() {
-        var current = 1, total = this.imageEls.length;
+    proto.updateMeta = function() {
+        var current = 1, total = this.imageEls.length, container = this.container;
         this.imageEls.some(function(el, i) {
             if (el.cls.contains('active')) {
                 current = i + 1;
@@ -33,12 +36,12 @@ var GalleryManager = (function(Q) {
             }
             return false;
         });
-        this.container.data('current', current);
-        this.container.data('total', total);
-        this.container.data('position', current === 1 ? 'left' : current === total ? 'right' : 'middle');
+        container.data('current', current);
+        container.data('total', total);
+        container.data('position', current === 1 ? 'left' : current === total ? 'right' : 'middle');
     };
 
-    Gallery.prototype.attachEvent = function() {
+    proto.attachEvent = function() {
         var gallery = this;
         this.leftEl.on('img', 'click', function(ev) {
             gallery.move('left');
@@ -48,18 +51,16 @@ var GalleryManager = (function(Q) {
         });
     };
 
-    Gallery.prototype.update = function(toActiveEl) {
+    proto.update = function(toActiveEl) {
         Gallery.deactivate(this.imageEls);
         Gallery.activate(toActiveEl || this.imageEls[0]);
         this.updateMeta();
         this.updateIndicator();
     };
-    Gallery.prototype.updateIndicator = function() {
-        var current, total;
-        current = this.container.data('current');
-        total = this.container.data('total');
+    proto.updateIndicator = function() {
+        var container = this.container;
 
-        this.indicatorEl.el.innerHTML = current + '/' + total;
+        this.indicatorEl.el.innerHTML = container.data('current') + '/' + container.data('total');
     };
 
     Gallery.deactivate = function(els) {
@@ -76,8 +77,7 @@ var GalleryManager = (function(Q) {
 
     return {
         init: function() {
-            var galleries = Q.all('.gallery-container');
-            galleries.forEach(function(el) {
+            Q.all('.gallery-container').forEach(function(el) {
                 return new Gallery(el);
             });
         }
