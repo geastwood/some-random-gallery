e501079d99a3facbedd9895a1789c01da6324673
Fei Liu <liu.feiwood@gmail.com>
2015-08-25 18:36:34 +0200

    wrap query.js with IIFE and optimize it for minification

--- js/query.js
+++ js/query.js
@@ -1,54 +1,61 @@
-var protoMap = Array.prototype.map;
-var Query = function(node) {
-    this.el = node;
-    this.events = {};
-};
-Query.prototype.attr = function(attr, value) {
-    /* jshint eqnull: true */
-    if (value != null) {
-        this.el.setAttribute(attr, value);
-    }
-    return this.el.getAttribute(attr);
-};
-Query.prototype.data = function(str, value) {
-    return this.attr('data-' + str, value);
-};
-Query.prototype.all = function(selector) {
-    return this.el ? Query.all(selector, this.el) : null;
-};
-Query.prototype.find = function(selector) {
-    return this.el ? Query.find(selector, this.el) : null;
-};
-Object.defineProperty(Query.prototype, 'cls', {
-    get: function() {
-        return this.el ? this.el.classList : [];
-    }
-});
-Query.prototype.on = function(selector, event, fn) {
-    var targets = this.all(selector),
-    cb = function(ev) {
-        if (targets.some(function(el) {return el.el === ev.target;})) {
-            fn.call(this, ev);
+var Query = (function() {
+    var Query = function(node) {
+        this.el = node;
+        this.events = {};
+    };
+    var proto = Query.proto;
+    var protoMap = Array.prototype.map;
+
+    proto.attr = function(attr, value) {
+        var el = this.el;
+        /* jshint eqnull: true */
+        if (value != null) {
+            el.setAttribute(attr, value);
         }
-    }.bind(this);
-    this.el.addEventListener(event, cb, false);
+        return el.getAttribute(attr);
+    };
+    proto.data = function(str, value) {
+        return this.attr('data-' + str, value);
+    };
+    proto.all = function(selector) {
+        return this.el ? Query.all(selector, this.el) : null;
+    };
+    proto.find = function(selector) {
+        return this.el ? Query.find(selector, this.el) : null;
+    };
+    Object.defineProperty(proto, 'cls', {
+        get: function() {
+            return this.el ? this.el.classList : [];
+        }
+    });
+    proto.on = function(selector, event, fn) {
+        var targets = this.all(selector),
+        cb = function(ev) {
+            if (targets.some(function(el) {return el.el === ev.target;})) {
+                fn.call(this, ev);
+            }
+        }.bind(this);
+        this.el.addEventListener(event, cb, false);
 
-    this.events[event] = cb;
-};
-Query.prototype.off = function(event) {
-    if (this.events[event]) {
-        this.el.removeEventListener(event, this.events[event]);
-        this.events[event] = null;
-    }
-};
+        this.events[event] = cb;
+    };
+    proto.off = function(event) {
+        if (this.events[event]) {
+            this.el.removeEventListener(event, this.events[event]);
+            this.events[event] = null;
+        }
+    };
 
-Query.all = function(selector, context) {
-    return protoMap.call((context || document).querySelectorAll(selector), Query.create);
-};
-Query.find = function(selector, context) {
-    var rst = protoMap.call([(context || document).querySelector(selector)], Query.create);
-    return rst[0] || null;
-};
-Query.create = function(node) {
-    return new Query(node);
-};
+    Query.all = function(selector, context) {
+        return protoMap.call((context || document).querySelectorAll(selector), Query.create);
+    };
+    Query.find = function(selector, context) {
+        var rst = protoMap.call([(context || document).querySelector(selector)], Query.create);
+        return rst[0] || null;
+    };
+    Query.create = function(node) {
+        return new Query(node);
+    };
+
+    return Query;
+}());
